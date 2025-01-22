79b79ca3fa6db827711986824e35db7693de065f
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-22 17:42:19 -0400

    Added 2nd class

---
+++ comit-java/src/main/java/org/hw/comit_java/course/hw2.java
@@ -0,0 +1,13 @@
+package org.hw.comit_java.course;
+
+public class hw2 {
+public static void main(String[] args) {
+	 // Declare and initialize two double variables
+    double num1 = 45.62435;
+    double num2 = 107.25424;
+
+    // Print the variables to the console
+    System.out.println("The value of num1 is: " + num1);
+    System.out.println("The value of num2 is: " + num2);
+}
+}
