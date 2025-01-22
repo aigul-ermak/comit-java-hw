b5a7e8c7194af4f774464a768ef3544e5d497354
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-22 17:47:14 -0400

    Added 4th class

---
+++ comit-java/src/main/java/org/hw/comit_java/course/hw4.java
@@ -0,0 +1,12 @@
+package org.hw.comit_java.course;
+
+public class hw4 {
+	public static void main(String[] args) {
+		String message1 = "I love Java";
+		String message2 = "Hello ComIT";
+
+		// Print the variables to the console
+		System.out.println(message1);
+		System.out.println(message2);
+	}
+}
