63da38dce8b44533aa4b0cb1464006798156d7b4
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-22 17:40:08 -0400

    Created maven project and added 1st class

---
+++ comit-java/src/main/java/org/hw/comit_java/course/hw1.java
@@ -0,0 +1,13 @@
+package org.hw.comit_java.course;
+
+public class hw1 {
+	public static void main(String[] args) {
+		  // Declare and initialize two int variables
+        int num1 = 23452;
+        int num2 = 45363;
+
+        // Print the variables to the console
+        System.out.println("The value of num1 is: " + num1);
+        System.out.println("The value of num2 is: " + num2);
+	}
+}
