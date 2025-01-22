17b7934ffa9d4b86d2728719af644ad957a87f26
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-22 17:44:59 -0400

    Added 3d class

---
+++ comit-java/src/main/java/org/hw/comit_java/course/hw3.java
@@ -0,0 +1,12 @@
+package org.hw.comit_java.course;
+
+public class hw3 {
+public static void main(String[] args) {
+	 long num1 = 24125456143L;
+     long num2 = 784125345656L;
+
+     // Print the variables to the console
+     System.out.println("The value of num1 is: " + num1);
+     System.out.println("The value of num2 is: " + num2);
+}
+}
