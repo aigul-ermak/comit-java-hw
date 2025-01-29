7c34e967f25d83d57d1abc91212e45422ff5b47c
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-29 18:47:23 -0400

    added hw1 sum

---
+++ comit-java/src/main/java/org/hw/comit_java/course/_02_practice/hw1_sum.java
@@ -0,0 +1,11 @@
+package org.hw.comit_java.course._02_practice;
+
+public class hw1_sum {
+	public static void main(String[] args) {
+		int num1 = 1234;
+		int num2 = 532;
+		int sum = num1 + num2;
+
+		System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+	}
+}
--- comit-java/src/main/java/org/hw/comit_java/course/hw5.java
+++ comit-java/src/main/java/org/hw/comit_java/course/hw5.java
@@ -2,11 +2,11 @@ package org.hw.comit_java.course;
 
 public class hw5 {
 	public static void main(String[] args) {
-		float num1 = 5.3f; 
-        float num2 = 7.5f;
+		float num1 = 5.3f;
+		float num2 = 7.5f;
 
-        // Print the variables to the console
-        System.out.println("The value of num1 is: " + num1);
-        System.out.println("The value of num2 is: " + num2);
+		// Print the variables to the console
+		System.out.println("The value of num1 is: " + num1);
+		System.out.println("The value of num2 is: " + num2);
 	}
 }
