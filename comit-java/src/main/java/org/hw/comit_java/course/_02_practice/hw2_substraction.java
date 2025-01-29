1e72c5367e8ebf73fa8b2f8bf7df7d3329058a66
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-29 18:49:02 -0400

    added hw2 sub

---
+++ comit-java/src/main/java/org/hw/comit_java/course/_02_practice/hw2_substraction.java
@@ -0,0 +1,11 @@
+package org.hw.comit_java.course._02_practice;
+
+public class hw2_substraction {
+	public static void main(String[] args) {
+		int num1 = 1234;
+		int num2 = 532;
+		int difference = num1 - num2;
+
+		System.out.println("The subtraction of " + num1 + " and " + num2 + " is: " + difference);
+	}
+}
