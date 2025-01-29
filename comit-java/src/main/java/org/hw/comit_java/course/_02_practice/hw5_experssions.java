89bc64c209fd0820315dc4848ea55869269fc70a
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-29 18:57:30 -0400

    added hw7

--- comit-java/src/main/java/org/hw/comit_java/course/_02_practice/hw5_experssions.java
+++ comit-java/src/main/java/org/hw/comit_java/course/_02_practice/hw5_experssions.java
@@ -2,13 +2,13 @@ package org.hw.comit_java.course._02_practice;
 
 public class hw5_experssions {
 	public static void main(String[] args) {
-		
-		  System.out.println("(101 + 0) / 3 = " + ((101 + 0) / 3));
-	        System.out.println("3.0e-6 * 10000000.1 = " + (3.0e-6 * 10000000.1));
-	        System.out.println("true && true = " + (true && true));
-	        System.out.println("false && true = " + (false && true));
-	        System.out.println("(false && false) || (true && true) = " + ((false && false) || (true && true)));
-	        System.out.println("(false || false) && (true && true) = " + ((false || false) && (true && true)));
-	        
+
+		System.out.println("(101 + 0) / 3 = " + ((101 + 0) / 3));
+		System.out.println("3.0e-6 * 10000000.1 = " + (3.0e-6 * 10000000.1));
+		System.out.println("true && true = " + (true && true));
+		System.out.println("false && true = " + (false && true));
+		System.out.println("(false && false) || (true && true) = " + ((false && false) || (true && true)));
+		System.out.println("(false || false) && (true && true) = " + ((false || false) && (true && true)));
+
 	}
 }
---
+++ comit-java/src/main/java/org/hw/comit_java/course/_02_practice/hw7_print_sum.java
@@ -0,0 +1,10 @@
+package org.hw.comit_java.course._02_practice;
+
+public class hw7_print_sum {
+	 public static void main(String[] args) {
+	        int a = 25;
+	        int sum = (a / 10) + (a % 10);
+	        
+	        System.out.println("The sum of digits of " + a + " is: " + sum);
+	    }
+}
