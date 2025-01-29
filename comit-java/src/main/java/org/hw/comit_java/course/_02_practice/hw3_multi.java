624c8a43f0c0a22ae4be571c5cb7b003573fa071
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-29 18:50:40 -0400

    added hw3 mult

---
+++ comit-java/src/main/java/org/hw/comit_java/course/_02_practice/hw3_multi.java
@@ -0,0 +1,11 @@
+package org.hw.comit_java.course._02_practice;
+
+public class hw3_multi {
+	public static void main(String[] args) {
+		 int num1 = 1234;
+	        int num2 = 532;
+	        int product = num1 * num2;
+	        
+	        System.out.println("The multiplication of " + num1 + " and " + num2 + " is: " + product);
+	}
+}
