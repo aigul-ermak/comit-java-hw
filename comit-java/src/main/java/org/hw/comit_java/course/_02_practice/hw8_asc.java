207d0a4d10ff17d42ab207bd9864a9d864f67f6c
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-29 18:58:20 -0400

    added hw8

---
+++ comit-java/src/main/java/org/hw/comit_java/course/_02_practice/hw8_asc.java
@@ -0,0 +1,10 @@
+package org.hw.comit_java.course._02_practice;
+
+public class hw8_asc {
+	public static void main(String[] args) {
+		char a = 'A';
+		int asciiValue = (int) a;
+
+		System.out.println("The ASCII value of '" + a + "' is: " + asciiValue);
+	}
+}
