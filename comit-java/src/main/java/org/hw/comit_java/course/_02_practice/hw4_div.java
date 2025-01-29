d08daf983e0c2b495fa10a0c8a0fe68913855791
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-01-29 18:51:47 -0400

    added hw4 div

---
+++ comit-java/src/main/java/org/hw/comit_java/course/_02_practice/hw4_div.java
@@ -0,0 +1,11 @@
+package org.hw.comit_java.course._02_practice;
+
+public class hw4_div {
+	public static void main(String[] args) {
+		int num1 = 1234;
+		int num2 = 532;
+		double quotient = (double) num1 / num2;
+
+		System.out.println("The division of " + num1 + " by " + num2 + " is: " + quotient);
+	}
+}
