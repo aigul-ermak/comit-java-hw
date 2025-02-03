7c132fee7b4b06c85cd8ab2c350bc5caf02a1114
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-03 15:17:24 -0400

    Added hw11

---
+++ comit-java/src/main/java/org/hw/comit_java/course/_03_practice/hw11_printHello.java
@@ -0,0 +1,9 @@
+package org.hw.comit_java.course._03_practice;
+
+public class hw11_printHello {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 200; i++) {
+			System.out.println("Hello");
+		}
+	}
+}
