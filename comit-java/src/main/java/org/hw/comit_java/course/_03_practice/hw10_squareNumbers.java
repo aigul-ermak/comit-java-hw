442b84ded44cd6765fd2aae3494d124e7d477048
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-03 15:16:02 -0400

    Added hw10

---
+++ comit-java/src/main/java/org/hw/comit_java/course/_03_practice/hw10_squareNumbers.java
@@ -0,0 +1,11 @@
+package org.hw.comit_java.course._03_practice;
+
+public class hw10_squareNumbers {
+	public static void main(String[] args) {
+		System.out.println("Squares of the first 30 natural numbers:");
+
+		for (int i = 1; i <= 30; i++) {
+			System.out.println(i + "² = " + (i * i));
+		}
+	}
+}
