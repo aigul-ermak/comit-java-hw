9ed0354fc5881964eda5831eb39e2a516cbf3619
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-03 15:10:29 -0400

    Added hw6

---
+++ comit-java/src/main/java/org/hw/comit_java/course/_03_practice/hw6_printNumbers.java
@@ -0,0 +1,10 @@
+package org.hw.comit_java.course._03_practice;
+
+public class hw6_printNumbers {
+	public static void main(String[] args) {
+		// Loop to print numbers from 1 to 3
+		for (int i = 1; i <= 3; i++) {
+			System.out.println(i);
+		}
+	}
+}
