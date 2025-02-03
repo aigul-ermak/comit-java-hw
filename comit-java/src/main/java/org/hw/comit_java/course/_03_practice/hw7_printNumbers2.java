9edaf0c2c82b7c18a736234428fdd6785a0018bf
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-03 15:11:30 -0400

    Added hw7

---
+++ comit-java/src/main/java/org/hw/comit_java/course/_03_practice/hw7_printNumbers2.java
@@ -0,0 +1,10 @@
+package org.hw.comit_java.course._03_practice;
+
+public class hw7_printNumbers2 {
+	  public static void main(String[] args) {
+	        // Loop to print numbers from 1 to 10,000
+	        for (int i = 1; i <= 10000; i++) {
+	            System.out.println(i);
+	        }
+	    }
+}
